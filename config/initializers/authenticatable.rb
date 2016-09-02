6a04e134d1ac6e10cf316240974b743d7f7c47fb
Frank Wu <workhard_smile@163.com>
2016-09-03 00:01:20 +0800

    update user config

--- config/initializers/authenticatable.rb
+++ config/initializers/authenticatable.rb
@@ -1,6 +1,7 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 if rails_env == "production"
-  $authenticate_method = :ldap_authenticatable
+  # $authenticate_method = :ldap_authenticatable
+  $authenticate_method = :database_authenticatable
 else
   $authenticate_method = :database_authenticatable
 end

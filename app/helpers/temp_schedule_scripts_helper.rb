b9d23282a6541679a2fda5ac19e31240755a9e5f
Gang Wu <gang.wu@istuary.com>
2016-09-20 14:13:47 +0800

    add wait_time for call mysql procdure.

--- app/helpers/temp_schedule_scripts_helper.rb
+++ app/helpers/temp_schedule_scripts_helper.rb
@@ -4,11 +4,17 @@ module TempScheduleScriptsHelper
     platforms.each do |platform|
       project_names.each do |project_name|
         ActiveRecord::Base.connection.execute("call get_schedule_scripts_by_tnumber_and_project_and_platform(#{threads_number},'#{project_name}','#{platform['type']}','#{platform['version']}','#{operation_system['type']}','#{operation_system['version']}','#{slave_name}','#{remote_ip}')")
+        wait_sql = "select count(*) num from information_schema.PROCESSLIST where STATE='executing' and INFO like '%get_schedule_scripts_by_tnumber_and_project_and_platform%'"
+        10.times.each do |i|
+          count = ActiveRecord::Base.connection.execute(wait_sql)[0]["num"]
+          break if count <= 1
+          sleep(3)
+        end
         temp_schedule_scripts = TempScheduleScript.find_all_by_project_name_and_platform_and_ip_and_deleted(project_name,platform['type'],remote_ip,0)||[]
         temp_schedule_scripts.each do |temp_schedule_script|
-          temp_schedule_script.deleted = 1
-          temp_schedule_script.save!
           schedule_scripts = schedule_scripts<<temp_schedule_script
+          temp_schedule_script.deleted = 1
+          temp_schedule_script.save!          
         end
       end
     end

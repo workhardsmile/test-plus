7abe706e0b3653b1228d9d825076a0cffd425963
Gang Wu <gang.wu@istuary.com>
2016-09-26 13:56:41 +0800

    update slave project name logics.

--- app/controllers/temp_schedule_scripts_controller.rb
+++ app/controllers/temp_schedule_scripts_controller.rb
@@ -2,7 +2,12 @@ class TempScheduleScriptsController < InheritedResources::Base
   #respond_to :js
   def get_schedule_scripts
     puts params[:salve_name],params['salve_name']
-    schedule_scripts = TempScheduleScriptsHelper.get_schedule_scripts(params[:salve_name],params[:platforms],params[:project_names],params[:threads_number].to_i,params[:operation_system],request.remote_ip)
-    render :json => schedule_scripts
+    slave = Slave.find_all_by_name(params[:salve_name])[0] rescue nil
+    schedule_scripts = []
+    unless slave.nil?
+      params[:project_names] = params[:project_names].select{|p_name| slave.project_name.include?('*') or slave.project_name.include?(p_name)}
+      schedule_scripts = TempScheduleScriptsHelper.get_schedule_scripts(params[:salve_name],params[:platforms],params[:project_names],params[:threads_number].to_i,params[:operation_system],request.remote_ip)
+    end
+    render :json => schedule_scripts    
   end
 end

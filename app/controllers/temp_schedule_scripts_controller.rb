d37b09552f316e6febb5c9b02b0194d8cef1fe4f
Frank Wu <gang.wu@istuary.com>
2017-06-09 13:36:01 +0800

    update testplus remote slave ip.

--- app/controllers/temp_schedule_scripts_controller.rb
+++ app/controllers/temp_schedule_scripts_controller.rb
@@ -6,7 +6,7 @@ class TempScheduleScriptsController < InheritedResources::Base
     schedule_scripts = []
     unless slave.nil?
       params[:project_names] = params[:project_names].select{|p_name| slave.project_name.include?('*') or slave.project_name.include?(p_name)}
-      schedule_scripts = TempScheduleScriptsHelper.get_schedule_scripts(params[:salve_name],params[:platforms],params[:project_names],params[:threads_number].to_i,params[:operation_system],request.remote_ip)
+      schedule_scripts = TempScheduleScriptsHelper.get_schedule_scripts(params[:salve_name],params[:platforms],params[:project_names],params[:threads_number].to_i,params[:operation_system], (params[:ip] || request.remote_ip) )
     end
     render :json => schedule_scripts    
   end
